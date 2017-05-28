e7ef0352e1fa1b143a0ea5d0d7ee830ec1025ec5
sfediukovich <sfediukovich@deltixlab.com>
2017-05-28 15:18:08 +0300

    add MotionPeriodDefinition

---
+++ src/main/java/model/MotionPeriodDefinition.java
@@ -0,0 +1,100 @@
+package model;
+
+import java.security.InvalidParameterException;
+
+public class MotionPeriodDefinition {
+
+    private int sampleId;
+    private int motionId;
+    private OriginalMotionType originalMotionType;
+    private int startTime;
+    private int endTime;
+    private int duration;
+
+    public MotionPeriodDefinition(int sampleId, int motionId, OriginalMotionType originalMotionType, int startTime, int endTime, int duration) {
+
+        if(null == originalMotionType)
+            throw new InvalidParameterException("originalMotionType is not defined. [originalMotionType can not be null");
+
+        this.sampleId = sampleId;
+        this.motionId = motionId;
+        this.originalMotionType = originalMotionType;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.duration = duration;
+    }
+
+    public int getSampleId() {
+        return sampleId;
+    }
+
+    public void setSampleId(int sampleId) {
+        this.sampleId = sampleId;
+    }
+
+    public int getMotionId() {
+        return motionId;
+    }
+
+    public void setMotionId(int motionId) {
+        this.motionId = motionId;
+    }
+
+    public OriginalMotionType getOriginalMotionType() {
+        return originalMotionType;
+    }
+
+    public void setOriginalMotionType(OriginalMotionType originalMotionType) {
+        this.originalMotionType = originalMotionType;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MotionPeriodDefinition that = (MotionPeriodDefinition) o;
+
+        if (sampleId != that.sampleId) return false;
+        if (motionId != that.motionId) return false;
+        if (startTime != that.startTime) return false;
+        if (endTime != that.endTime) return false;
+        if (duration != that.duration) return false;
+        return originalMotionType == that.originalMotionType;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = sampleId;
+        result = 31 * result + motionId;
+        result = 31 * result + originalMotionType.hashCode();
+        result = 31 * result + startTime;
+        result = 31 * result + endTime;
+        result = 31 * result + duration;
+        return result;
+    }
+}
---
+++ src/main/java/model/OriginalMotionType.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum OriginalMotionType {
+    BACKHAND_FLAT
+    //TODO: Add OriginalMotionType
+}
