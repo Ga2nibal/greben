073c32ffc285469d88914d38c2a2a46aae00aa1a
sfediukovich <sfediukovich@deltixlab.com>
2017-06-22 01:26:40 +0300

    bind controller and trainingButton

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -1,4 +1,15 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button trainingButton;
+
+    @FXML
+    public void onClicktrainingButtonMethod(){
+        trainingButton.setText("Thanks!");
+    }
 }
